88c013472ef398df0a242d67eed001fb6469257c
hqudmx <1341879458@qq.com>
2016-06-05 18:58:13 +0800

    /********2016--6-05*********/ 增加地图开发

---
+++ app/src/main/java/f_app/dmx/com/securelocate/MyMap.java
@@ -0,0 +1,47 @@
+package f_app.dmx.com.securelocate;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.MapView;
+
+/**
+ * Created by Administrator on 2016/6/5.
+ */
+public class MyMap extends Activity {
+    private MapView mMapView;
+
+    private AMap amap;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        setContentView(R.layout.activity_map);
+
+        mMapView=(MapView) findViewById(R.id.map);
+
+        mMapView.onCreate(savedInstanceState);
+        amap=mMapView.getMap();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+}
