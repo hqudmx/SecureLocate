b72c7829730d332313a69fb52e58434440abee29
hqudmx <1341879458@qq.com>
2016-06-09 22:10:02 +0800

    /********2016--6--09*********/ 更新主页面布局以及功能实现

--- app/src/main/java/f_app/dmx/com/securelocate/NewHomeActivity.java
+++ app/src/main/java/f_app/dmx/com/securelocate/NewHomeActivity.java
@@ -2,15 +2,21 @@ package f_app.dmx.com.securelocate;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
 import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
+
+import java.util.ArrayList;
 
 /**
  * Created by Administrator on 2016/6/4.
@@ -45,9 +51,68 @@ public class NewHomeActivity extends Activity{
         instance=this;
         mTabPager= (ViewPager) findViewById(R.id.tabpager);
         mTabPager.setOnPageChangeListener(new MyOnPageChangeListener());
+
+        mTab1= (ImageView) findViewById(R.id.image_map);
+        mTab2= (ImageView) findViewById(R.id.image_location);
+        mTab3= (ImageView) findViewById(R.id.img_humiture);
+        mTab4= (ImageView) findViewById(R.id.image_setting);
+
+        mTabImg=(ImageView)findViewById(R.id.img_tab_now);
+
+        mTab1.setOnClickListener(new MyOnClickListener(0));
+        mTab2.setOnClickListener(new MyOnClickListener(1));
+        mTab3.setOnClickListener(new MyOnClickListener(2));
+        mTab4.setOnClickListener(new MyOnClickListener(3));
+
+        Display currDisplay=getWindowManager().getDefaultDisplay();
+        int displayWidth=currDisplay.getWidth();
+        int displayHeight=currDisplay.getHeight();
+        one=displayWidth/4;
+        two=one*2;
+        three=one*3;
+
+        LayoutInflater mLi=LayoutInflater.from(this);
+         View view1=mLi.inflate(R.layout.activity_map,null);
+        View view2=mLi.inflate(R.layout.activity_alarm,null);
+        View view3=mLi.inflate(R.layout.activity_humiture,null);
+        View view4=mLi.inflate(R.layout.activity_setting,null);
+        final ArrayList<View> views= new ArrayList<View>();
+        views.add(view1);
+        views.add(view2);
+        views.add(view3);
+        views.add(view4);
+
+        PagerAdapter mPagerAdapter=new PagerAdapter() {
+            @Override
+            public int getCount() {
+                return views.size();
+            }
+
+            @Override
+            public boolean isViewFromObject(View view, Object object) {
+                return view==object;
+            }
+
+            @Override
+            public void destroyItem(ViewGroup container, int position, Object object) {
+                ((ViewPager)container).removeView(views.get(position));
+            }
+
+            @Override
+            public Object instantiateItem(View container, int position) {
+                ((ViewPager)container).addView(views.get(position));
+                return  views.get(position);
+            }
+        };
+        mTabPager.setAdapter(mPagerAdapter);
+
+
+
+
     }
 
     private class MyOnPageChangeListener implements ViewPager.OnPageChangeListener {
+
         @Override
         public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
 
@@ -58,20 +123,68 @@ public class NewHomeActivity extends Activity{
             Animation animation=null;
             switch (position){
                 case 0:
+                    if(currIndex==1){
+                        animation=new TranslateAnimation(one,0,0,0);
+                    }else  if(currIndex==2){
+                        animation=new TranslateAnimation(two,0,0,0);
+                    }else if(currIndex==3){
+                        animation=new TranslateAnimation(three,0,0,0);
+                    }
                     break;
                 case 1:
+                    if(currIndex==1){
+                        animation=new TranslateAnimation(zero,one,0,0);
+                    }else  if(currIndex==2){
+                        animation=new TranslateAnimation(two,one,0,0);
+                    }else if(currIndex==3){
+                        animation=new TranslateAnimation(three,one,0,0);
+                    }
                     break;
                 case 2:
+                    if(currIndex==1){
+                        animation=new TranslateAnimation(zero,two,0,0);
+                    }else  if(currIndex==2){
+                        animation=new TranslateAnimation(one,two,0,0);
+                    }else if(currIndex==3){
+                        animation=new TranslateAnimation(three,two,0,0);
+                    }
                     break;
                 case 3:
+                    if(currIndex==1){
+                        animation=new TranslateAnimation(zero,three,0,0);
+                    }else  if(currIndex==2){
+                        animation=new TranslateAnimation(one,three,0,0);
+                    }else if(currIndex==3){
+                        animation=new TranslateAnimation(two,three,0,0);
+                    }
                     break;
 
             }
+
+            currIndex=position;
+            animation.setFillAfter(true);
+            animation.setDuration(150);
+            mTabPager.startAnimation(animation);
 
         }
 
         @Override
         public void onPageScrollStateChanged(int state) {
+
+        }
+    }
+
+    /**
+     * 图标点击事件
+     */
+    private class MyOnClickListener implements View.OnClickListener {
+        private int index=0;
+        MyOnClickListener(int i){
+            index=i;
+        }
+        @Override
+        public void onClick(View v) {
+            mTabPager.setCurrentItem(index);
 
         }
     }
